7ce17ba5f17e7c26da4653adbe9dc81ad90db832
renan-am <recam94@gmail.com>
2017-07-21 21:38:14 +0000

    add func distancia e protege

--- IA_func.c
+++ IA_func.c
@@ -3,6 +3,7 @@
 #include <func_declarations.h>
 
 
+
 int distancia (int l_origem, int tipo){  //retorna quantas linhas até a pedra virar damas (fim do tabuleiro)
 	if (tipo == 1)
 		return l_origem; 
@@ -12,3 +13,28 @@ int distancia (int l_origem, int tipo){  //retorna quantas linhas até a pedra v
 		printf ("func distancia recebeu tipo invalido (%d)", int tipo);
 		return -1;
 }
+
+int protege (BOARD *tab, int c_origem, int l_origem){   //retorna numero de pedras aliadas protegidas por uma pedra nessa posição
+	int tipo;
+	tipo = tab[l_origem*COL_MAX+c_origem].tipo;
+
+	if (tipo != 0)
+		tipo = (tipo%2) + 1;   // converte 1 -> 2 e 2 -> 1 para checarSeCome pode funcionar.
+	int count = 0;
+
+	if (checaSeCome (tab, c_origem, l_origem, c_origem+1, l_origem+1, tipo))
+		count++;
+	if (checaSeCome (tab, c_origem, l_origem, c_origem-1, l_origem+1, tipo))
+		count++;
+	if (checaSeCome (tab, c_origem, l_origem, c_origem-1, l_origem-1, tipo))
+		count++;
+	if (checaSeCome (tab, c_origem, l_origem, c_origem+1, l_origem-1, tipo))
+		count++;
+
+	return count;
+}
+
+
+
+
+
